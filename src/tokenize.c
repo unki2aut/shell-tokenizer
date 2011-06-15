b27d20c096732857e3e9f4a3efbea8b2b5a65e6b
Martin Fillafer <unki2aut@gmail.com>
2011-06-15 10:51:40 +0200

    extended readme, some testing and fixing of improper use

--- src/tokenize.c
+++ src/tokenize.c
@@ -1,10 +1,10 @@
-/***************************************************************/
-/* FILENAME: cut2.c                                            */
-/* PURPOSE:  splitting a source by delimiter                   */
-/* AUTHOR:   Fillafer Martin (unki2aut@gmail.com)              */
-/* DATE:     07.06.2009                                        */
-/* VERSION:  v0.1                                              */
-/***************************************************************/
+/******************************************************************************/
+/* FILENAME: tokenize.c                                                       */
+/* PURPOSE:  tokenize a source by delimiter                                   */
+/* AUTHOR:   (c) Martin Fillafer (unki2aut@gmail.com)                         */
+/* DATE:     2011-06-15                                                       */
+/* VERSION:  v0.2                                                             */
+/******************************************************************************/
 
 #include <stdio.h>
 #include <getopt.h>
@@ -14,83 +14,98 @@
 #include <assert.h>
 
 #define MAXLINE 1024
-#define DEBUG 0
 
 /* globals */
 const char* szCommand = "<not yet set>";
 
 void usage(void) {
-	(void) fprintf(stderr, "Usage: %s -d DELIM [- | filename]\n", szCommand);
-	exit(EXIT_FAILURE);
+  (void) fprintf(stderr, "Usage: %s -d <delim> [- | filename]\n", szCommand);
+  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv) {
-	FILE *fp;
-	char buffer[MAXLINE];
-	char *delim, *file;
-	int ibuf = 0, idelim = 0, i = 0;
+  FILE *fp;
+  char buffer[MAXLINE];
+  char *delim = NULL, *file = NULL;
+  int ibuf = 0, idelim = 0, i = 0;
 
-	szCommand = argv[0];
+  szCommand = argv[0];
 
-	if (argc < 2 && argc > 3) {
-		usage();
-	}
+#ifdef DEBUG
+  fprintf(stdout, "DEBUG argc: %i\n", argc);
+#endif
 
-	if (strcmp(argv[1], "-d") != 0) {
-		usage();
-	}
+  if (argc < 3 || argc > 4) {
+    usage();
+  }
 
-	delim = argv[2];
-	file = argv[3];
+  if (strcmp(argv[1], "-d") != 0) {
+    usage();
+  }
 
-	if (DEBUG) {
-		fprintf(stdout, "delim: %s\n\n", delim);
-	}
+  delim = argv[2];
 
-	if (file == NULL) {
-		fp = stdin;
-	} else if (strcmp(file, "-") == 0) {
-		fp = stdin;
-	} else if ((fp = fopen(file, "r")) == 0) {
-		fp = stdin;
-	}
+  if(argc == 4) {
+    file = argv[3];
+  }
 
-	while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-		ibuf = 0;
-		idelim = 0;
+#ifdef DEBUG
+  fprintf(stdout, "DEBUG delim: '%s'\n", delim);
+#endif
 
-		while (ibuf < sizeof(buffer) && buffer[ibuf] != '\0') {
-			if (delim[idelim] == buffer[ibuf] && idelim != strlen(delim)) {
-				if (DEBUG)
-					fprintf(stdout, "\nbuf[%i]: %c, delim[%i]: %c\n", ibuf,
-							buffer[ibuf], idelim, delim[idelim]);
+  if (file == NULL) {
+    fp = stdin;
+#ifdef DEBUG
+    printf("DEBUG file == NULL\n");
+#endif
+  } else if (strcmp(file, "-") == 0) {
+    fp = stdin;
+#ifdef DEBUG
+    printf("DEBUG file == '-'\n");
+#endif
+  } else if ((fp = fopen(file, "r")) == 0) {
+    fp = stdin;
+    usage();
+  }
 
-				idelim++;
-				ibuf++;
-			} else {
-				/* if characters are not the same */
-				if (idelim == 0) {
-					fprintf(stdout, "%c", buffer[ibuf]);
-					ibuf++;
-				} else {
-					if (DEBUG)
-						fprintf(stdout, "\nidelim: %i, strlen(delim): %i\n",
-								idelim, strlen(delim));
+  while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    ibuf = 0;
+    idelim = 0;
 
-					if (idelim == strlen(delim)) { /* if delimiter was found */
-						fprintf(stdout, "\n");
+    while (ibuf < sizeof(buffer) && buffer[ibuf] != '\0') {
+      if (delim[idelim] == buffer[ibuf] && idelim != strlen(delim)) {
+#ifdef DEBUG
+        printf("\nDEBUG buf[%i]: %c, delim[%i]: %c\n",
+          ibuf, buffer[ibuf], idelim, delim[idelim]);
+#endif
 
-					} else { /* if parts of delim are the same */
-						for (i = ibuf - idelim; i < ibuf; i++)
-							fprintf(stdout, "%c", buffer[i]);
-					}
-				}
+        idelim++;
+        ibuf++;
+      } else {
+        /* if characters are not the same */
+        if (idelim == 0) {
+          (void) fprintf(stdout, "%c", buffer[ibuf]);
+          ibuf++;
+        } else {
+#ifdef DEBUG
+          printf("DEBUG idelim: %i, strlen(delim): %i",
+            idelim, strlen(delim));
+#endif
 
-				idelim = 0;
-			}
-		}
-	}
+          if (idelim == strlen(delim)) { /* if delimiter was found */
+            (void) fprintf(stdout, "\n");
 
-	fclose(fp);
-	exit(EXIT_SUCCESS);
+          } else { /* if parts of delim are the same */
+            for (i = ibuf - idelim; i < ibuf; i++)
+              (void) fprintf(stdout, "%c", buffer[i]);
+          }
+        }
+
+        idelim = 0;
+      }
+    }
+  }
+
+  fclose(fp);
+  exit(EXIT_SUCCESS);
 }
